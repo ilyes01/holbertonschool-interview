9c5c0bc36743eba10dd0b6f5e56d3a5b8fad87b0
ilyes01 <ilyesghrab7@gmail.com>
2021-12-06 12:56:01 +0100

    locked boxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'Lockboxe'
+def canUnlockAll(boxes):
+    """
+    list of lists(boxes)
+    key number of keys
+    return true if all boxes opened else it's false
+    """
+    if (len(boxes) < 1 or type(boxes) is not list):
+        return False
+    open_box = [0]
+    i = 0
+    while i < len(boxes):
+        if i >= len(open_box):
+            return False
+        if boxes[open_box[i]] is not None:
+            for key in boxes[open_box[i]]:
+                if key not in open_box and key < len(boxes):
+                    open_box.append(key)
+        i += 1
+    if len(open_box) == len(boxes):
+        return True
+    else:
+        return False
