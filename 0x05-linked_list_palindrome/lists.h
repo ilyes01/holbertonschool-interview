b7ed203311208e00d9aa76769821bdd5a4083f40
ilyes01 <ilyesghrab7@gmail.com>
2022-03-01 13:11:30 +0100

    update

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -9,12 +9,12 @@
  */
 typedef struct listint_s
 {
-    int i;
+    int n;
     struct listint_s *next;
 }
 listint_t;
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int i);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 
