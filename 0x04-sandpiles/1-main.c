45790e037762d2d45cf45d582e3f0e352a746c44
ilyes01 <ilyesghrab7@gmail.com>
2022-03-04 14:42:18 +0100

    mainfile

---
+++ 0x04-sandpiles/1-main.c
@@ -0,0 +1,15 @@
+/**
+*main - Entry point
+*Return: EXIT_SUCCESS or EXIT_FAILURE
+*/
+Same as 0-main.c except:
+int grid1[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+    int grid2[3][3] = {
+        {3, 3, 3},
+        {3, 3, 3},
+        {3, 3, 3}
+    };
