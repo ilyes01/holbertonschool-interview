d47d2c429c5aebf8ac65db7d6a689fd64d51f360
ilyes01 <ilyesghrab7@gmail.com>
2022-07-11 10:37:27 +0200

    n queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -13,7 +13,7 @@ try:
 except ValueError:
     print("N must be a number")
     exit(1)
-"""n should be bigger than four"""
+"""n should be bigger than  four"""
 if b < 4:
     print("N must be at least 4")
     exit(1)
