7adc629d411ac94e9cb382d532d80f0ae89b85ed
ilyes01 <ilyesghrab7@gmail.com>
2022-07-11 10:52:35 +0200

    fiixxx

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,43 +1,28 @@
 #!/usr/bin/python3
-"""function Nqueens"""
-
 import sys
-
-
 if len(sys.argv) != 2:
-    print("Usage: nqueens N")
+    print('Usage: nqueens N')
     exit(1)
-try:
-    board = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
+if sys.argv[1].isdigit() is False:
+    print('N must be a number')
     exit(1)
-if board < 4:
-    print("N must be at least 4")
+N = int(sys.argv[1])
+if (N < 4):
+    print('N must be at least 4')
     exit(1)
-
-
-def chest(board, place1, solders):
-    """nqueens function"""
-    for x in range(board):
-        col = 0
-        for quen in solders:
-            if x == quen[1]:
-                col = 1
-                break
-            if place1 - x == quen[0] - quen[1]:
-                col = 1
-                break
-            if x - quen[1] == quen[0] - place1:
-                col = 1
-                break
-        if col == 0:
-            solders.append([place1, x])
-            if place1 != board - 1:
-                chest(board, place1 + 1, solders)
-            else:
-                print(solders)
-            del solders[-1]
-
-
-chest(board, 0, [])
+a = 0
+frequence_a = 2
+for i in range(N - 2):
+    Lists = []
+    a += 1
+    n = a
+    for b in range(N):
+        Lists.append([b, a])
+        if (a + frequence_a > N-1):
+            a += frequence_a - 1 - N
+        else:
+            a += frequence_a
+    print(Lists)
+    frequence_a += 1
+    a = n
+exit(0)
