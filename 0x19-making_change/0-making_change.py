039c96b5dc2f2a7589bc1ff8726837ecebce7b21
ilyes01 <ilyesghrab7@gmail.com>
2022-08-02 15:15:01 +0200

    fixxit

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -5,13 +5,19 @@
 def makeChange(coins, total):
     """fewest number of coin"""
 
-    c = 0
-    coins = sorted(coins, reverse=True)
-    for ele in coins:
-        if total // ele != 0:
-            c += total // ele
-            total = total % ele
+    if total <= 0:
+        return 0
+    if not coins or min(coins) > total:
+        return -1
+    n_coins = 0
+    coins.sort(reverse=True)
+    x = 0
+    while x < len(coins):
+        if coins[x] <= total:
+            n_coins += total // coins[x]
+            total = total % coins[x]
+        x += 1
     if total == 0:
-        return c
+        return n_coins
     else:
         return -1
