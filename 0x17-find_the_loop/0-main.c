07539d4852e57c0ff160474b479bbca2f5407527
ilyes01 <ilyesghrab7@gmail.com>
2022-08-02 14:04:07 +0200

    main

--- 0x17-find_the_loop/0-main.c
+++ 0x17-find_the_loop/0-main.c
@@ -1,29 +1,53 @@
-alex@~/0x0B-menger$ cat 0-main.c 
-#include <stdio.h>
 #include <stdlib.h>
-
-#include "menger.h"
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
 /**
- * main - Entry point
+ * main - check the code for Holberton School students.
  *
- * @ac: Arguments counter
- * @av: Arguments vector
- *
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * Return: Always 0.
  */
-int main(int ac, char **av)
+int main(void)
 {
-    int level;
+    listint_t *head;
+    listint_t *head2;
+    listint_t *node;
 
-    if (ac < 2)
+    head2 = NULL;
+    add_nodeint(&head2, 0);
+    add_nodeint(&head2, 1);
+    add_nodeint(&head2, 2);
+    add_nodeint(&head2, 3);
+    add_nodeint(&head2, 4);
+    add_nodeint(&head2, 98);
+    add_nodeint(&head2, 402);
+    add_nodeint(&head2, 1024);
+    print_listint_safe(head2);
+    node = find_listint_loop(head2);
+    if (node != NULL)
     {
-        fprintf(stderr, "Usage: %s level\n", av[0]);
-        return (EXIT_FAILURE);
+        printf("Loop starts at [%p] %d\n", (void *)node, node->n);
     }
-
-    level = atoi(av[1]);
-    menger(level);
-
-    return (EXIT_SUCCESS);
+    free_listint_safe(&head2);
+    head = NULL;
+    node = add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    add_nodeint(&head, 5);
+    add_nodeint(&head, 6);
+    node->next = add_nodeint(&head, 7);
+    add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint_safe(head);
+    node = find_listint_loop(head);
+    if (node != NULL)
+    {
+        printf("Loop starts at [%p] %d\n", (void *)node, node->n);
+    }
+    free_listint_safe(&head);
+    return (0);
 }
