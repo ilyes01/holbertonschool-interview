86299d70c13082a6e28ba93db93b26318bf456b9
ilyes01 <ilyesghrab7@gmail.com>
2022-08-02 14:59:36 +0200

    fixxx it

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
 """
- calculate how many square units of water will be retained after it rains.
+calculate how many square units of water will
+be retained after it rains
 """
-
 
 
 def rain(walls):
-"""
-function rain to cal the water
-"""
+    """
+    rain function
+    """
+    if walls is None or walls == []:
+        return 0
+    if len(walls) == 1:
+        return 0
 
+    a = len(walls)
+    left_side = [0] * a
 
-    rainAmount = 0
-    rainFill = 0
-    wallsLength = len(walls)
-    if (wallsLength == 0):
-        return (0)
+    right_side = [0] * a
+    wat = 0
 
-    for i in range(1, wallsLength - 1):
-        leftPeak = walls[i]
-        rightPeak = walls[i]
-        for L in range(0, i):
-            if (walls[L] > leftPeak):
-                leftPeak = walls[L]
-        for R in range(i + 1, wallsLength):
-            if (walls[R] > rightPeak):
-                rightPeak = walls[R]
-        rainFill = min(leftPeak, rightPeak) - walls[i]
-        rainAmount += rainFill
+    left_side[0] = walls[0]
+    for x in range(1, a):
+        left_side[x] = max(left_side[x - 1], walls[x])
 
-    return (rainAmount)
+    right_side[a - 1] = walls[a - 1]
+    for x in range(a - 2, -1, -1):
+        right_side[x] = max(right_side[x + 1], walls[x])
+
+    for x in range(0, a):
+        wat += min(left_side[x], right_side[x]) - walls[x]
+
+    return wat
