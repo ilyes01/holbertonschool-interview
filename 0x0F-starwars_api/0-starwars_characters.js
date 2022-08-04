f8ab03cf6225fb972525d6bf4b1e2afcfe8c5621
ilyes01 <ilyesghrab7@gmail.com>
2022-08-04 11:34:29 +0200

    updatee

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,10 +4,9 @@ const request = require('request');
 const url = `https://swapi.dev/api/films/${movieId}/`;
 const movieId = process.argv[2];
 request(url, async (error, response, body) => {
-  if (movieId == 0) return;
+  if (movieId == null) return;
   if (error) console.log(error);
-
-  for (let i = 0; i < JSON.parse(body).characters.length; i++) {
+  for (let i = 0; i < JSON.parse(body).characters.length; i+1) {
     await new Promise((resolve, reject) => {
       request(JSON.parse(body).characters[i], function (error, response, body) {
         if (error) console.log(error);
@@ -16,4 +15,5 @@ request(url, async (error, response, body) => {
 });
     });
   }
+
 });
