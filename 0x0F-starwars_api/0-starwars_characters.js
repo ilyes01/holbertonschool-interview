89d8276abbf4d7e146092d1b40078dba696c68e7
ilyes01 <ilyesghrab7@gmail.com>
2022-08-04 11:33:10 +0200

    fixx

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -7,7 +7,7 @@ request(url, async (error, response, body) => {
   if (movieId == 0) return;
   if (error) console.log(error);
 
-  for (let i = 0; i < JSON.parse(body).characters.length; i+1) {
+  for (let i = 0; i < JSON.parse(body).characters.length; i++) {
     await new Promise((resolve, reject) => {
       request(JSON.parse(body).characters[i], function (error, response, body) {
         if (error) console.log(error);
