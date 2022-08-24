ed6cc17b66650e81a6509c407647c8fb5af793ee
root <ilyesghrab7@gmail.com>
2022-08-24 07:11:14 -0700

    update

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
 import sys
+
 if len(sys.argv) != 2:
-    print('Usage: nqueens N')
+    print("Usage: nqueens N")
     exit(1)
-if sys.argv[1].isdigit() is False:
-    print('N must be a number')
+try:
+    b = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
     exit(1)
-N = int(sys.argv[1])
-if (N < 4):
-    print('N must be at least 4')
+if b < 4:
+    print("N must be at least 4")
     exit(1)
-a = 0
-frequence_a    a += 1
-    n = a
-    for b in range(N):
-        Lists.append([b, a])
-        if (a + frequence_a > N-1):
-            a += frequence_a - 1 - N
-        else:
-            a += frequence_a
-    print(Lists)
-    frequence_a += 1
-    a = n
-exit(0)
+
+
+def chest(b, place1, sold):
+    for x in range(b):
+        c = 0
+        for quen in sold:
+            if x == quen[1]:
+                c = 1
+                break
+            if place1 - x == quen[0] - quen[1]:
+                c = 1
+                break
+            if x - quen[1] == quen[0] - place1:
+                c = 1
+                break
+        if c == 0:
+            sold.append([place1, x])
+            if place1 != b - 1:
+                chest(b, place1 + 1, sold)
+            else:
+                print(sold)
+            del sold[-1]
+
+
+chest(b, 0, [])
