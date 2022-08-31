dbc36877466df582f3501727c88694bfcc19a60d
ilyes01 <ilyesghrab7@gmail.com>
2022-08-31 16:51:28 +0200

    update

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,10 +6,10 @@ def rotate_2d_matrix(matrix):
     """rotate 2d matrix"""
     x = 0
     while x < len(matrix[0]):
-        for j in range(x, len(matrix[0])):
-            aux = matrix[x][j]
-            matrix[x][j] = matrix[j][x]
-            matrix[j][x] = aux
+        for i in range(x, len(matrix[0])):
+            aux = matrix[x][i]
+            matrix[x][i] = matrix[i][x]
+            matrix[i][x] = aux
         x += 1
-    for j in range(len(matrix[0])):
-        matrix[j].reverse()
+    for i in range(len(matrix[0])):
+        matrix[i].reverse()
