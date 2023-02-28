381fb901b73e9448fb0b8672388678986274261c
ilyes01 <ilyesghrab7@gmail.com>
2023-02-28 18:26:17 +0100

    betttty

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -3,66 +3,71 @@
 #include "search.h"
 
 /**
- * valcheck -  checker between prev and tmp
- * @p: node
- * @current: node
- * @n: what we searchnig for
- * Return: contains the value or NULL
+ * valcheck - checks if a node contains a given value
+ * @p: node to start searching from
+ * @current: node to stop searching at
+ * @n: value to search for
+ *
+ * Return: pointer to node containing the value, or NULL if not found
  */
 skiplist_t *valcheck(skiplist_t *p, skiplist_t *current, int n)
 {
-	for (; p != current->next; p = p->next)
-	{
-		if (p->n == n)
-			return (p);
-	}
-	return (NULL);
+    while (p != current->next)
+    {
+        if (p->n == n)
+            return (p);
+        p = p->next;
+    }
+    return (NULL);
 }
+
 /**
  * linear_skip - searches sorted skip list of integers
- * @list:  pointer
- * @value: the value we searching for
- * Return: a pointer
+ * @list: pointer to the head of the skip list
+ * @value: value to search for
+ *
+ * Return: pointer to node containing the value, or NULL if not found
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *current = list, *p = list;
+    skiplist_t *current = list, *p = list;
 
-	while (current)
-	{
-		if ((current->n < value
-		|| ((current->n == value) && !(valcheck(p, current, value))))
-		&& current->next)
-		{
-			if (current->index)
-				printf("Value checked at index [%lu] = [%d]\n",
-				current->index, current->n);
-			p = current;
-			if (!current->express)
-			{
-				while (current->next)
-					current = current->next;
-			}
-			else
-				current = current->express;
-		}
-		else
-		{
-			if (p->n > value)
-				return (NULL);
-			if (current->n >= value)
-				printf("Value checked at index [%lu] = [%d]\n", current->index,
-				current->n);
-			printf("Value found between indexes [%lu] and [%lu]\n",
-					p->index, current->index);
-			for (; p && p != current->next; p = p->next)
-			{
-				printf("Value checked at index [%lu] = [%d]\n", p->index, p->n);
-				if (p->n == value)
-					return (p);
-			}
-			return (NULL);
-		}
-	}
-	return (NULL);
+    while (current)
+    {
+        if ((current->n < value ||
+             (current->n == value && !valcheck(p, current, value))) &&
+            current->next)
+        {
+            if (current->index)
+                printf("Value checked at index [%lu] = [%d]\n",
+                       current->index, current->n);
+            p = current;
+            if (!current->express)
+            {
+                while (current->next)
+                    current = current->next;
+            }
+            else
+                current = current->express;
+        }
+        else
+        {
+            if (p->n > value)
+                return (NULL);
+            if (current->n >= value)
+                printf("Value checked at index [%lu] = [%d]\n",
+                       current->index, current->n);
+            printf("Value found between indexes [%lu] and [%lu]\n",
+                   p->index, current->index);
+            while (p && p != current->next)
+            {
+                printf("Value checked at index [%lu] = [%d]\n", p->index, p->n);
+                if (p->n == value)
+                    return (p);
+                p = p->next;
+            }
+            return (NULL);
+        }
+    }
+    return (NULL);
 }
