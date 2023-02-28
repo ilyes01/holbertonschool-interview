f2db36269d9099186593f7d35cc07f106dd0a19c
ilyes01 <ilyesghrab7@gmail.com>
2023-02-28 17:52:29 +0100

    fiiix

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -3,73 +3,65 @@
 #include "search.h"
 
 /**
- * linear_skip - performs linear skip search on a sorted linked list
- *
- * @list: head of the sorted list
- * @value: the value to search for
- *
- * Return: pointer to the node containing the searched value or NULL on failure
+ * valceck -  checker between prev and tmp
+ * @: node
+ * @current: node
+ * @n: what we searchnig for
+ * Return: contains the value or NULL
+ */
+skiplist_t *valcheck(skiplist_t *p, skiplist_t *current, int n)
+{
+    for (; p != current->next; p = p->next)
+    {
+        if (p->n == n)
+            return (p);
+    }
+    return (NULL);
+}
+/**
+ * linear_skip - searches sorted skip list of integers
+ * @list:  pointer
+ * @value: the value we searching for
+ * Return: a pointer
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	/* Initialize two pointers to the head of the list */
-	skiplist_t *explore = list, *current = list;
+	skiplist_t *current = list, *p = list;
 
-	/* Check if the list is empty */
-	if (!list)
-		return (NULL);
-
-	/* Traverse the list in jumps until reaching the value or end of the list */
-	for (; explore->express != NULL; explore = explore->express)
+	while (current)
 	{
-		current = explore;
-
-		/* Print the checked value at each index during the traversal */
-		printf("Value checked at index [%ld] = [%d]\n", explore->index, explore->n);
-
-		/* If the current value is greater or equal to the search value or
-		 * we've reached the end of the list, perform linear search on the
-		 * sublist between the current and previous pointers */
-		if (explore->n >= value)
+		if ((current->n < value || ((current->n == value) && !(valcheck(p, current, value))))
+		&& current->next)
 		{
-			/* Print the index range of the sublist we're performing linear search on */
-			printf("Value found between indexes [%ld] and [%ld]\n", current->index, explore->index);
-
-			/* Perform linear search on the sublist and return the node containing
-			 * the searched value or NULL if the value is not found */
-			for (; current != explore->next; current = current->next)
+			if (current->index)
+				printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+			p = current;
+			if (!current->express)
 			{
-				printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
-				if (current->n == value)
-					return (current);
+				while (current->next)
+					current = current->next;
 			}
-
+			else
+			{
+				current = current->express;
+			}
+		}
+		else
+		{
+			if (p->n > value)
+				return (NULL);
+			if (current->n >= value)
+				printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+			printf("Value found between indexes [%lu] and [%lu]\n",
+					p->index, current->index);
+			for (; p && p != current->next; p = p->next)
+{
+    printf("Value checked at index [%lu] = [%d]\n", p->index, p->n);
+    if (p->n == value)
+        return p;
+}
 			return (NULL);
 		}
 	}
-
-	/* If we've reached the end of the list and the current value
-	 * is less than the search value, move the explore pointer to the
-	 * last node in the list */
-	if (explore->n < value)
-	{
-		current = explore;
-		for (; explore->next != NULL; explore = explore->next)
-			continue;
-	}
-
-	/* Print the index range of the sublist we're performing linear search on */
-	printf("Value found between indexes [%ld] and [%ld]\n", current->index, explore->index);
-
-	/* Perform linear search on the sublist and return the node containing
-	 * the searched value or NULL if the value is not found */
-	for (; current != explore->next; current = current->next)
-	{
-		printf("Value checked at index [%ld] = [%d]\n", current->index, current->n);
-		if (current->n == value)
-			return (current);
-	}
-
-	/* If we reach the end of the list without finding the value, return NULL */
 	return (NULL);
 }
