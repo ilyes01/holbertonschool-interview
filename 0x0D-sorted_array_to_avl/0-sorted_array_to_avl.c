16cb0d005be4fe701dfd8a48cf7393da609a4a72
ilyes01 <ilyesghrab7@gmail.com>
2023-03-12 14:36:11 +0100

    betty fixing

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,29 +10,29 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *tree;
-    size_t middle;
+	avl_t *tree;
+	size_t middle;
 
-    if (!array || size == 0)
-        return (NULL);
+	if (!array || size == 0)
+		return (NULL);
 
-    /* Allocate memory for the new node */
-    tree = malloc(sizeof(avl_t));
-    if (!tree)
-        return (NULL);
+	/* Allocate memory for the new node */
+	tree = malloc(sizeof(avl_t));
+	if (!tree)
+		return (NULL);
 
-    /* Find the middle element and make it the root */
-    middle = size / 2;
-    tree->n = array[middle];
-    tree->parent = NULL;
+	/* Find the middle element and make it the root */
+	middle = size / 2;
+	tree->n = array[middle];
+	tree->parent = NULL;
 
-    /* Recursively construct the left and right subtrees */
-    tree->left = sorted_array_to_avl(array, middle);
-    if (tree->left)
-        tree->left->parent = tree;
-    tree->right = sorted_array_to_avl(array + middle + 1, size - middle - 1);
-    if (tree->right)
-        tree->right->parent = tree;
+	/* Recursively construct the left and right subtrees */
+	tree->left = sorted_array_to_avl(array, middle);
+	if (tree->left)
+		tree->left->parent = tree;
+	tree->right = sorted_array_to_avl(array + middle + 1, size - middle - 1);
+	if (tree->right)
+		tree->right->parent = tree;
 
-    return (tree);
+	return (tree);
 }
