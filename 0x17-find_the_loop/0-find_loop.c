74c972918aa6688b03dd0384f1385c388844a1a2
ilyes01 <ilyesghrab7@gmail.com>
2023-03-14 16:02:56 +0100

    betty fixing

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -9,32 +9,32 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *tortoise, *hare;
+	listint_t *tortoise, *hare;
 
-    if (head == NULL || head->next == NULL)
-        return (NULL);
+		if (head == NULL || head->next == NULL)
+		return (NULL);
 
-    tortoise = head->next;
-    hare = head->next->next;
+	tortoise = head->next;
+	hare = head->next->next;
 
-    while (hare && hare->next)
-    {
-        if (tortoise == hare)
-        {
-            tortoise = head;
+	while (hare && hare->next)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
 
-            while (tortoise != hare)
-            {
-                tortoise = tortoise->next;
-                hare = hare->next;
-            }
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
 
-            return (tortoise);
-        }
+			return (tortoise);
+		}
 
-        tortoise = tortoise->next;
-        hare = hare->next->next;
-    }
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
