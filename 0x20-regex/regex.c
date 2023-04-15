8e8c276258a3ce164cb1a51bb2a7b55c74d2b5e7
ilyes01 <ilyesghrab7@gmail.com>
2023-04-15 21:54:15 +0100

    update

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -3,25 +3,28 @@
 #include "regex.h"
 
 /**
- * regex_match - checks whether a given pattern matches a given string.
+ * regex_match - checks whether a given pattern matches a given string
  * @str: the string to scan
  * @pattern: the regular expression
  *
- * Return: 1 if the pattern matches the string, or 0 if it doesn’t
+ * Return: 1 if the pattern matches the string, or 0 if it doesn't
  */
 int regex_match(char const *str, char const *pattern)
 {
 	if (*pattern == '\0')
 		return (*str == '\0');
 
-	if (*(pattern + 1) == '*') {
+	if (*(pattern + 1) == '*')
+	{
 		if (regex_match(str, pattern + 2))
 			return (1);
 
-		while (*str != '\0' && (*str == *pattern || *pattern == '.')) {
+		while (*str != '\0' && (*str == *pattern || *pattern == '.'))
+		{
 			if (regex_match(++str, pattern + 2))
 				return (1);
 		}
+
 		return (0);
 	}
 
@@ -33,3 +36,4 @@ int regex_match(char const *str, char const *pattern)
 
 	return (0);
 }
+
