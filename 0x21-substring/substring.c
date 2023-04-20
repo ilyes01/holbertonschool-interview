2d523692f79f54ec19cd85931403dbef880aec21
ilyes01 <ilyesghrab7@gmail.com>
2023-04-20 16:41:38 +0100

    finaaalllyyyy

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -1,48 +1,58 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "substring.h"
+
+/**
+ * find_substring - finds all the possible substrings containing a list of
+ * words, within a given string.
+ * @s: the string to scan
+ * @words: the array of words all substrings must be a concatenation
+ * arrangement of
+ * @nb_words: the number of elements in the array words
+ * @n: holds the address at which to store the number of elements in the
+ * returned array.
+ *
+ * Return: an allocated array, storing each index in s, at which a substring
+ * was found. If no solution is found, NULL can be returned
+ */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-    int slen = strlen(s);
-    int wlen = strlen(words[0]);
-    int total_len = nb_words * wlen;
-    int *freq = calloc(nb_words, sizeof(int));
-    int *indices = malloc(slen * sizeof(int));
-    int i, j, k, start, count, match;
-    *n = 0;
+    int len, word_len, remaining, j, i, k;
+    int *count, *res;
 
-    for (i = 0; i <= slen - total_len; i++) {
-        memset(freq, 0, nb_words * sizeof(int));
-        count = 0;
-        for (j = 0; j < nb_words; j++) {
-            start = i + j * wlen;
-            match = 0;
-            for (k = 0; k < wlen; k++) {
-                if (s[start + k] != words[j][k]) {
+    if (!s || !words || !*words || !n || nb_words < 1)
+        return (NULL);
+
+    len = strlen(s);
+    word_len = strlen(words[0]);
+    count = calloc(nb_words, sizeof(int));
+    res = malloc(len * sizeof(int));
+
+    for (i = 0, *n = 0; i <= len - nb_words * word_len; i++)
+    {
+        remaining = nb_words;
+        for (j = 0; j < nb_words; j++)
+        {
+            for (k = 0; k < nb_words; k++)
+            {
+                if (count[k] == 0 && strncmp(s + i + j * word_len, words[k], word_len) == 0)
+                {
+                    count[k] = 1;
+                    remaining--;
                     break;
                 }
-                match++;
             }
-            if (match == wlen) {
-                freq[j]++;
-                count++;
-            }
+            if (k == nb_words)
+                break;
         }
-        if (count == nb_words) {
-            indices[*n] = i;
-            (*n)++;
-        } else {
-            for (j = 0; j < nb_words; j++) {
-                freq[j] = 0;
-            }
-        }
+        if (remaining == 0)
+            res[(*n)++] = i;
+        memset(count, 0, nb_words * sizeof(int));
     }
 
-    free(freq);
-    if (*n == 0) {
-        free(indices);
-        indices = NULL;
-    }
-    return indices;
+    free(count);
+
+    return ((*n) > 0 ? res : NULL);
 }
 
