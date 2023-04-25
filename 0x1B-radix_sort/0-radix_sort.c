b5e363c311030d47c7e6a886763a7ccddd8a1f83
ilyes01 <ilyesghrab7@gmail.com>
2023-04-25 21:59:45 +0100

    update for betty

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -11,43 +11,43 @@
  */
 void radix_sort(int *array, size_t size)
 {
-    size_t i, exp = 1;
-    int max_value = 0;
-    int *output = NULL;
+	size_t i, exp = 1;
+	int max_value = 0;
+	int *output = NULL;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    output = malloc(sizeof(int) * size);
-    if (output == NULL)
-        return;
+	output = malloc(sizeof(int) * size);
+	if (output == NULL)
+		return;
 
-    for (i = 0; i < size; i++)
-        max_value = array[i] > max_value ? array[i] : max_value;
+	for (i = 0; i < size; i++)
+		max_value = array[i] > max_value ? array[i] : max_value;
 
-    while (max_value / exp > 0)
-    {
-        size_t bucket[10] = {0};
+	while (max_value / exp > 0)
+	{
+		size_t bucket[10] = {0};
 
-        for (i = 0; i < size; i++)
-            bucket[array[i] / exp % 10]++;
+		for (i = 0; i < size; i++)
+			bucket[array[i] / exp % 10]++;
 
-        for (i = 1; i < 10; i++)
-            bucket[i] += bucket[i - 1];
+		for (i = 1; i < 10; i++)
+			bucket[i] += bucket[i - 1];
 
-        for (i = size - 1; i < SIZE_MAX; i--)
-        {
-            output[bucket[array[i] / exp % 10] - 1] = array[i];
-            bucket[array[i] / exp % 10]--;
-        }
+		for (i = size - 1; i < SIZE_MAX; i--)
+		{
+			output[bucket[array[i] / exp % 10] - 1] = array[i];
+			bucket[array[i] / exp % 10]--;
+		}
 
-        for (i = 0; i < size; i++)
-            array[i] = output[i];
+		for (i = 0; i < size; i++)
+		array[i] = output[i];
 
-        exp *= 10;
-        print_array(array, size);
-    }
+		exp *= 10;
+		print_array(array, size);
+	}
 
-    free(output);
+	free(output);
 }
 
