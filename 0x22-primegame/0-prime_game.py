fb5558ba9e0c8e84db5ae9e5319e72164c3f54a3
ilyes01 <ilyesghrab7@gmail.com>
2023-04-27 13:09:24 +0100

    prime game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,45 +1,42 @@
 #!/usr/bin/python3
-""" primegame """
 
-
-def isPrime(x):
-    """ checks if a number is prime """
-    if x < 2:
+def is_prime(n):
+    """is prime"""
+    if n < 2:
         return False
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
     return True
 
 
-def isWinner(x, nums):
-    """ prime game """
-    if x < 1 or not nums or nums == []:
-        return None
-    r = min(x, len(nums))
-    maria_wins = 0
-    ben_wins = 0
-    for r_i in range(r):
-        n = list(range(2, nums[r_i] + 1))
-        is_maria = True
-        while n:
-            p = n.pop(0)
-            is_prime = isPrime(p)
-            has_primes = False
-            for i in range(p, nums[r_i] + 1, p):
-                if i in n:
-                    n.remove(i)
-                    has_primes = True
-            if not has_primes:
-                break
-            is_maria = not is_maria if is_prime else is_maria
-        if is_maria:
-            maria_wins += 1
-        else:
-            ben_wins += 1
+def get_primes(n):
+    """get prime"""
 
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
+    primes = set()
+    for i in range(2, n + 1):
+        if is_prime(i):
+            primes.add(i)
+    return primes
+
+
+def isWinner(x, nums):
+
+    """is winner"""
+    winners = []
+    for n in nums:
+        primes = get_primes(n)
+        if len(primes) == 0:
+            winners.append(None)
+        elif len(primes) % 2 == 0:
+            winners.append('Ben')
+        else:
+            winners.append('Maria')
+    ben_wins = winners.count('Ben')
+    maria_wins = winners.count('Maria')
+    if ben_wins > maria_wins:
         return 'Ben'
-    return None
+    elif maria_wins > ben_wins:
+        return 'Maria'
+    else:
+        return None
