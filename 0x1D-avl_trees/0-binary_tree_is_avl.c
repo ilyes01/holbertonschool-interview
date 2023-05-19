498e8a22b31925f9cd9f167c5c8916c9f1247950
ilyes01 <ilyesghrab7@gmail.com>
2023-05-19 18:19:16 +0100

    update betty

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -10,15 +10,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    return (1 + (left_height > right_height ? left_height : right_height));
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
 
 /**
@@ -31,14 +31,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_bst(const binary_tree_t *tree, int min, int max)
 {
-    if (tree == NULL)
-        return (1);
+	if (tree == NULL)
+		return (1);
 
-    if (tree->n < min || tree->n > max)
-        return (0);
+	if (tree->n < min || tree->n > max)
+		return (0);
 
-    return (binary_tree_is_bst(tree->left, min, tree->n - 1) &&
-            binary_tree_is_bst(tree->right, tree->n + 1, max));
+	return (binary_tree_is_bst(tree->left, min, tree->n - 1) &&
+			binary_tree_is_bst(tree->right, tree->n + 1, max));
 }
 
 /**
@@ -49,23 +49,23 @@ int binary_tree_is_bst(const binary_tree_t *tree, int min, int max)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    int left_height, right_height, height_diff;
+	int left_height, right_height, height_diff;
 
-    if (tree == NULL)
-        return (1);
+	if (tree == NULL)
+		return (1);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-    height_diff = abs(left_height - right_height);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	height_diff = abs(left_height - right_height);
 
-    if (height_diff > 1)
-        return (0);
+	if (height_diff > 1)
+		return (0);
 
-    if (!binary_tree_is_bst(tree, INT_MIN, INT_MAX))
-        return (0);
+	if (!binary_tree_is_bst(tree, INT_MIN, INT_MAX))
+		return (0);
 
-    if (!binary_tree_is_avl(tree->left) || !binary_tree_is_avl(tree->right))
-        return (0);
+	if (!binary_tree_is_avl(tree->left) || !binary_tree_is_avl(tree->right))
+		return (0);
 
-    return (1);
+	return (1);
 }
