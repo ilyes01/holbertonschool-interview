9be1ce324350b2309ff467c7d91d909e5f81f0ef
ilyes01 <ilyesghrab7@gmail.com>
2023-05-21 18:23:59 +0100

    update

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -32,7 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_is_bst(const binary_tree_t *tree, int min, int max)
 {
 	if (tree == NULL)
-		return (1);
+		return (0);
 
 	if (tree->n < min || tree->n > max)
 		return (0);
@@ -50,7 +50,7 @@ int binary_tree_is_bst(const binary_tree_t *tree, int min, int max)
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0);
+		return (1);
 
 	if (!binary_tree_is_bst(tree, INT_MIN, INT_MAX))
 		return (0);
